374ad47d1278411a14981c4c3053a2d009567108
ekaterinakuleshova <ekaterinakuleshova>
2022-07-01 15:01:51 +0000

    Order in na update

--- yellow_snow/orders_in_na.py
+++ yellow_snow/orders_in_na.py
@@ -61,88 +61,108 @@ def readJDBC(q, db):
 
 
 query = """
-SELECT DATE_FORMAT(o.created_at, "%Y-%m-%d %H:%i:%s.%f") AS order_created_date,
+SELECT DISTINCT 
+       o.created_at AS order_created_date,
        b.external_name AS merchant,
        pcz.title AS city_pcz,
        w.code AS terminal_code,
        ad.country_code AS terminal_country_code,
        pcz.country_code AS destination_country_code,
        btt.tag_id,
-       btt.buyer_id,
-       btt.name,
-       o.id AS orders,
-       p.id AS parcels,
-       p.visible,
-       'HOME' AS delivery_type,
-       DATE_FORMAT(utc_timestamp() , "%Y-%m-%d %H:%i:%s.%f") AS time_stamp
+       o.buyer_id,
+       o.id AS order_id,
+       p.id AS parcel_id,
+       IF(pcz.type = "TO_DOOR", 'HOME', 'BOX') AS delivery_type,
+       IF(p.visible = 1, last_sl.parcel_id, NULL) as scanned_parcel,
+       IF(p.visible = 1,distr_sl.parcel_id, NULL) as scanned_at_destination_terminal_parcel,
+       IF(p.visible = 1,destin_sl.parcel_id, NULL) as scanned_at_destination_country_parcel,
+       IF(p.visible = 1,nlbe_sl.parcel_id, NULL) as scanned_at_NL_BE_country_parcel
 FROM orders o USE INDEX(IDX_order_created_at) -- add index
-    JOIN postal_code_zones pcz ON o.delivery_postal_code_zone_id = pcz.id   
-        AND binary pcz.type = "TO_DOOR" -- information
-    JOIN buyers AS b ON b.id = o.buyer_id -- information
-    JOIN warehouses w ON pcz.terminal_id = w.id -- information
-    JOIN addresses ad ON w.address_id = ad.id -- information
-    LEFT JOIN parcels p on o.id = p.order_id -- information
-    LEFT JOIN (
-        SELECT 
-            tag_id, 
-            tag_type_id, 
-            t.name, 
-            bt.buyer_id
-        FROM tags t
-        JOIN buyer_tags bt on t.id = bt.tag_id
-        WHERE tag_id = 2) btt
-         ON btt.buyer_id = o.buyer_id -- information
+         JOIN postal_code_zones pcz ON o.delivery_postal_code_zone_id = pcz.id  -- information
+         JOIN buyers AS b ON b.id = o.buyer_id -- information
+         JOIN warehouses w ON pcz.terminal_id = w.id -- information
+         JOIN addresses ad ON w.address_id = ad.id -- information
+         JOIN parcels p ON o.id = p.order_id -- information
+         LEFT JOIN (
+                    SELECT
+                        tag_id,
+                        bt.buyer_id
+                    FROM tags t
+                             JOIN buyer_tags bt on t.id = bt.tag_id
+                    WHERE tag_id = 2) btt
+                   ON btt.buyer_id = o.buyer_id -- information
+          LEFT JOIN scanning_log last_sl ON last_sl.id = (
+                                                SELECT scanning_log.id
+                                                FROM scanning_log
+                                                WHERE scanning_log.parcel_id = p.id
+                                                ORDER BY scanning_log.id DESC
+                                                LIMIT 1
+                                                )
+
+         LEFT JOIN scanning_log distr_sl ON distr_sl.id = (
+                                                SELECT scanning_log.id
+                                                FROM scanning_log
+                                                JOIN users on scanning_log.user_id = users.id
+                                                JOIN user_settings on users.user_settings_id = user_settings.id
+                                                JOIN warehouses on user_settings.warehouse_id = warehouses.id
+                                                WHERE scanning_log.parcel_id = p.id
+                                                      AND warehouses.code = w.code
+                                                ORDER BY scanning_log.id DESC
+                                                LIMIT 1
+                                                )
+
+         LEFT JOIN scanning_log destin_sl ON destin_sl.id = (
+                                                SELECT scanning_log.id
+                                                FROM scanning_log
+                                                         JOIN users on scanning_log.user_id = users.id
+                                                         JOIN user_settings on users.user_settings_id = user_settings.id
+                                                         JOIN warehouses on user_settings.warehouse_id = warehouses.id
+                                                         JOIN addresses on warehouses.address_id = addresses.id
+                                                WHERE scanning_log.parcel_id = p.id
+                                                  AND addresses.country_code = pcz.country_code
+                                                ORDER BY scanning_log.id DESC
+                                                LIMIT 1
+                                                )
+
+         LEFT JOIN scanning_log nlbe_sl ON nlbe_sl.id = (
+                                                SELECT scanning_log.id
+                                                FROM scanning_log
+                                                         JOIN users on scanning_log.user_id = users.id
+                                                         JOIN user_settings on users.user_settings_id = user_settings.id
+                                                         JOIN warehouses on user_settings.warehouse_id = warehouses.id
+                                                         JOIN addresses on warehouses.address_id = addresses.id
+                                                WHERE scanning_log.parcel_id = p.id
+                                                  AND addresses.country_code in ('NL', 'BE')
+                                                ORDER BY scanning_log.id DESC
+                                                LIMIT 1
+                                                )
 WHERE o.created_at >= DATE_ADD(utc_date(), INTERVAL - 14 DAY)
-    AND o.cancellation_id IS NULL
-    AND o.id NOT IN (SELECT order_id FROM consignments WHERE consignments.cancellation_id IS NULL)
-    AND p.recall_requested_at IS NULL
-UNION ALL
-SELECT DATE_FORMAT(o.created_at, "%Y-%m-%d %H:%i:%s.%f") AS order_created_date,
-       b.external_name AS merchant,
-       pcz.title AS city_pcz,
-       w.code AS terminal_code,
-       ad.country_code AS terminal_country_code,
-       pcz.country_code AS destination_country_code,
-       btt.tag_id,
-       btt.buyer_id,
-       btt.name,
-       o.id AS orders,
-       p.id AS parcels,
-       p.visible,
-       'BOX' AS delivery_type,
-       DATE_FORMAT(utc_timestamp(), "%Y-%m-%d %H:%i:%s.%f") AS time_stamp
-FROM orders AS o USE INDEX(IDX_order_created_at) -- add index
-    JOIN postal_code_zones AS pcz ON o.delivery_postal_code_zone_id = pcz.id and binary pcz.type = "TO_LOCKER" -- information
-    JOIN buyers AS b ON b.id = o.buyer_id -- information
-    JOIN warehouses w ON pcz.terminal_id = w.id -- information
-    JOIN addresses ad ON w.address_id = ad.id -- information
-    LEFT JOIN parcels AS p ON o.id = p.order_id -- information
-    LEFT JOIN parcel_box_assignments AS pba ON p.id = pba.parcel_id -- information
-    LEFT JOIN consignments AS c ON c.order_id = o.id -- information
-    LEFT JOIN locker_consignments lc ON lc.order_id = o.id -- information
-    LEFT JOIN (
-        SELECT tag_id, tag_type_id, t.name, bt.buyer_id
-            FROM tags t
-            JOIN buyer_tags bt on t.id = bt.tag_id
-            WHERE tag_id = 2) btt
-        ON btt.buyer_id = o.buyer_id -- information
-WHERE  o.created_at >= DATE_ADD(utc_date(), INTERVAL - 14 DAY)
-    AND o.cancellation_id IS NULL
-    AND pba.id IS NULL
-    AND lc.id IS NULL
+  AND o.cancellation_id IS NULL
+  AND ((pcz.type = "TO_DOOR"
+       AND o.id NOT IN (SELECT order_id FROM consignments WHERE consignments.cancellation_id IS NULL)
+       AND p.recall_requested_at IS NULL)
+  OR (pcz.type = "TO_LOCKER"
+      AND p.id NOT IN (SELECT parcel_id FROM parcel_box_assignments)
+      AND o.id NOT IN (SELECT order_id FROM locker_consignments)
     AND o.buyer_id NOT IN ('476', '1050','1051','1052','1459')
     AND p.id NOT IN (
         SELECT p.id
         FROM routes AS r USE INDEX(IDX_due_date) -- add index
-        JOIN locker_pallets lp ON r.id = lp.route_id
-        JOIN locker_pallet_parcels lpp
-          ON lp.id = lpp.locker_pallet_id -- AND lp.locker_id = ls.locker_id
-        JOIN parcels p ON lpp.parcel_id = p.id
+                 JOIN locker_pallets lp ON r.id = lp.route_id
+                 JOIN locker_pallet_parcels lpp
+                      ON lp.id = lpp.locker_pallet_id -- AND lp.locker_id = ls.locker_id
+                 JOIN parcels p ON lpp.parcel_id = p.id
         WHERE r.type = "LOCKER"
-          AND r.due_date = utc_date())"""
+          AND r.due_date = utc_date())
+      ))"""
 
 orders_in_na_df = readJDBC(query, 'budbee')
 
 # COMMAND ----------
 
-writeSnowflake(orders_in_na_df, 'orders_in_na_WIP')
+orders_in_na_df_grouped = orders_in_na_df.groupBy("order_created_date","merchant", "city_pcz", "terminal_code", "terminal_country_code", "destination_country_code", "tag_id", "buyer_id", "delivery_type").agg(countDistinct("order_id").alias("orders"), countDistinct("parcel_id").alias("parcels"),countDistinct("scanned_parcel").alias("scanned_parcels"),
+                                   countDistinct("scanned_at_destination_terminal_parcel").alias("scanned_at_destination_terminal_parcels"),
+                                   countDistinct("scanned_at_destination_country_parcel").alias("scanned_at_destination_country_parcels"),
+                                   countDistinct("scanned_at_NL_BE_country_parcel").alias("scanned_at_NL_BE_country_parcels")        
+                                            )
+writeSnowflake(orders_in_na_df_grouped, 'orders_in_na_WIP')
