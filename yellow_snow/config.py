e91fe58ebbea75bfe5a2199789f5c2c181799ea7
jikrochmal <jikrochmal>
2022-07-04 07:48:18 +0000

    add a yellow snow configuration

---
+++ yellow_snow/config.py
@@ -0,0 +1,57 @@
+# Databricks notebook source
+import pyspark.sql.functions as F
+from pyspark.sql.functions import col, lit
+
+# COMMAND ----------
+
+snowflake_user = dbutils.secrets.get("poc-analyst", "databricks-snowflake-user")
+snowflake_password = dbutils.secrets.get("poc-analyst", "databricks-snowflake-password")
+snowflake_database = "dw_dev"
+snowflake_schema = "topaz"
+
+sf_options = {
+  "sfUrl": "bp67618.eu-west-1.snowflakecomputing.com"
+  , "sfUser": snowflake_user
+  , "sfPassword": snowflake_password
+  , "sfDatabase": snowflake_database
+  , "sfSchema": snowflake_schema
+  , "sfWarehouse": "dbricks_wh"
+}
+
+def readSnowflake(table):
+    return spark.read \
+  .format("snowflake") \
+  .options(**sf_options) \
+  .option("dbtable", table) \
+  .load()
+
+def writeSnowflake(df, tablename):
+
+    df.write \
+    .format('snowflake') \
+    .options(**sf_options) \
+    .option("dbtable", tablename) \
+    .mode('overwrite') \
+    .save()
+
+# COMMAND ----------
+
+jdbcHostname = "production-aurora-db-03.c24yqmofl8oi.eu-west-1.rds.amazonaws.com"
+jdbcPort = 3306
+
+# TODO: refactor these into metastore to avoid plaintext passwords
+jdbcUsername = dbutils.secrets.get("poc-analyst", "budbee-db-user")
+jdbcPassword = dbutils.secrets.get("poc-analyst", "budbee-db-password")
+
+def readJDBC(q, db):
+    readConfig = {
+        "user" : jdbcUsername
+        , "password" : jdbcPassword
+        , "driver" : "com.mysql.jdbc.Driver"
+        , "url": f"jdbc:mysql://{jdbcHostname}:{jdbcPort}/{db}"
+        , "fetchsize": 2000
+        , "dbtable": f"({q}) as foo"
+    }
+    
+    return spark.read.format("jdbc").options(**readConfig).load()
+
